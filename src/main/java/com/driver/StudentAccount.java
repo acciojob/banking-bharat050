0824fca69c97a8458b419c622fd03b2bf3f8068d
Bharat <aimhigh050@gmail.com>
2024-01-11 00:36:43 +0530

    Access modifier change along with method creation

--- src/main/java/com/driver/StudentAccount.java
+++ src/main/java/com/driver/StudentAccount.java
@@ -2,7 +2,11 @@ package com.driver;
 
 public class StudentAccount extends BankAccount{
 
-    String  institutionName;
+    private String  institutionName;
+
+    public String getInstitutionName() {
+        return institutionName;
+    }
 
     public StudentAccount(String name, double balance, String  institutionName) {
         //minimum balance is 0 by default
